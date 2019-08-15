be82490c743c8eba6b98f59f65cc81182837e943
David Thong Nguyen <dev@davidtn.com>
2019-08-15 11:26:04 -0700

    Debugging and offline practice

--- pwa-training-labs-master/offline-quickstart-lab/app/service-worker.js
+++ pwa-training-labs-master/offline-quickstart-lab/app/service-worker.js
@@ -11,10 +11,9 @@ const precacheResources = [
 self.addEventListener('install', event => {
   console.log('Service worker install event!');
   event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(precacheResources);
-      })
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(precacheResources);
+    })
   );
 });
 
@@ -24,8 +23,8 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(caches.match(event.request)
-    .then(cachedResponse => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
       if (cachedResponse) {
         return cachedResponse;
       }
